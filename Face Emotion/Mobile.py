c25959669dc135de27138271cc8c7ade47f93192
Hariharan TS <hari04022005@gmail.com>
2024-07-26 21:05:59 +0530

    FIrst Commit

---
+++ Face Emotion/Mobile.py
@@ -0,0 +1,16 @@
+import urllib.request
+import cv2
+import numpy as np
+import imutils
+
+url='http://192.168.1.2:8080/shot.jpg'
+
+while True:
+    imgPath=urllib.request.urlopen(url)
+    imgNp=np.array(bytearray(imgPath.read()),dtype=np.uint8)
+    frame=cv2.imdecode(imgNp,-1)
+
+    frame=imutils.resize(frame,width=450)
+    cv2.imshow("frame",frame)
+    if ord('q')==cv2.waitKey(1):
+        exit(0)
